6e92f08cb2e2ce1ded4d97127aa3fe2781a953e4
Sarmad Sabih <sam_sarmad@hotmail.com>
2017-09-14 14:47:15 +0500

    changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def flash_alert_type(name)
+    case name.to_s
+      when "notice"
+        "success"
+      when "warning"
+        "warning"
+      when "info"
+        "info"
+      else
+        "danger"
+    end
+  end
 end
